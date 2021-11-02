876789249f7d4eed21eb64e70cf3cebb96f69bee
Shabinsky Dmitry <sdimosikvip@gmail.com>
2021-11-02 20:58:01 +0300

    init

--- app/src/androidTest/java/com/sdimosikvip/testui/test/SimpleTest.kt
+++ app/src/androidTest/java/com/sdimosikvip/testui/test/SimpleTest.kt
@@ -4,9 +4,17 @@ import androidx.test.rule.ActivityTestRule
 import com.sdimosikvip.testui.MainActivity
 import junit.framework.TestCase
 import org.junit.Rule
+import org.junit.Test
 
 
 class SimpleTest : TestCase() {
 
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    @Test
+    fun test() {
+        print("sdsd")
+    }
 
 }
